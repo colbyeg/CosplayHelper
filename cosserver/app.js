5662576cd8a349b60de88e5337f84bd47e4cd2a0
Colby Gavin <colbyeg@gmail.com>
2020-01-25 19:27:41 -0500

    configuring app to prepare for production

--- cosserver/app.js
+++ cosserver/app.js
@@ -1,4 +1,3 @@
-const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
@@ -7,30 +6,17 @@ const logger = require("morgan");
 const router = require("./routes");
 const app = express();
 
-// view engine setup
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.static(path.join(__dirname + "/../cosclient/build")));
 
 app.use("/", router);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../cosclient/build/index.html"));
 });
 
 module.exports = app;
